5a62c2ba6cca79030088ff3c9082082e7c4ae72f
judaharagao <judah.aragao@gmail.com>
2023-10-21 13:31:42 -0300

    config postgesql

--- src/main/java/com/fatextil/FaTextilApplication.java
+++ src/main/java/com/fatextil/FaTextilApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FaTextilApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FaTextilApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(FaTextilApplication.class, args);}
 
 }
