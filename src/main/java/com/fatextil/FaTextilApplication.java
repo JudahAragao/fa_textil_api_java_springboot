565573071e3187280170bdaea22e8b63db054965
judaharagao <judah.aragao@gmail.com>
2023-10-21 12:14:35 -0300

    start project

---
+++ src/main/java/com/fatextil/FaTextilApplication.java
@@ -0,0 +1,13 @@
+package com.fatextil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaTextilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaTextilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatextil/FaTextilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatextil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaTextilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
